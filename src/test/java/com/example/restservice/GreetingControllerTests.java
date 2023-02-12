12ca3b0ed4fe37261fd8aa337da68b6526e2c3ee
simon-towler <simon.towler@icloud.com>
2023-02-12 21:59:39 +0000

    Add unit test greeting_counter_increments

--- src/test/java/com/example/restservice/GreetingControllerTests.java
+++ src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -9,8 +9,19 @@ public class GreetingControllerTests {
         String template = "Hello, %s";
         String name = "Tupac Shakur";
         String expectedContent = String.format(template, name);
+
         GreetingController greetingController = new GreetingController();
         Greeting retVal = greetingController.greeting(name);
         assert retVal.content().equals(expectedContent);
+    }
+
+    @Test
+    void greeting_counter_increments() {
+        GreetingController greetingController = new GreetingController();
+        Greeting retVal1 = greetingController.greeting("");
+        long firstCounterValue = retVal1.id();
+        Greeting retVal2 = greetingController.greeting("");
+        long secondCounterValue = retVal2.id();
+        assert secondCounterValue == firstCounterValue + 1;
     }
 }
